a64461f366611de84469d8daf089d4738f1d5c1c
Maclemon17 <maclemon17@gmail.com>
2023-02-22 08:42:41 +0100

    added users and notes schemas

--- src/model/note.js
+++ src/model/note.js
@@ -1,9 +1,21 @@
 const mongoose = require("mongoose");
 
-const noteSchema = new mongoose.Schema(
-    {
-        
+const noteSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    note: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: mongoose.schema.Types.ObjectId,
+        ref: "User",
+        required: true,
     }
+
+}, { timestamps: true }
 )
 
 const Note = mongoose.model("Notes", noteSchema);
--- src/model/user.js
+++ src/model/user.js
@@ -1,14 +1,36 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema(
-    {
-        email: { type: String, required: true, unique: true },
-        username: { type: String, required: true, min: 3 },
-        password: { type: String, required: true },
-        profilePictureUrl: { type: String, default: " " },
-        notes: { type: Array },
-        hasAcceptedTerms: { type: Boolean, default: false }
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
     },
+    username: {
+        type: String,
+        required: true,
+        min: 3,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    profilePictureUrl: {
+        type: String,
+        default: ""
+    },
+    notes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Note"
+        }
+    ],
+    hasAcceptedTerms: {
+        type: Boolean,
+        default: false
+    }
+},
     { timestamps: true }
 );
 
